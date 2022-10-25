63745d5a4b2c82e1de03c19f3972917b00af27a8
AdamLenahanNclUni <c20346427@newcastle.ac.uk>
2022-10-25 10:47:15 +0100

    added withdrawal and deposit options to the customer class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Customer:
     """A class created to make a blueprint for customers of the bank"""
+
     def __init__(self, firstName, lastName, title, preferredPronouns, dateBorn, occupation, balance, overdraftLimit):
-        #initialising attributes for the class
+        # initialising attributes for the class
         self.f = firstName
         self.l = lastName
         self.t = title
@@ -11,5 +12,28 @@ class Customer:
         self.b = balance
         self.ov = overdraftLimit
 
+    def withdrawal(self, amount):
+        """lets the user withdraw an amount of money, catches any errors caused by going over overdraft limit"""
+        print("current amount in bank balance is ", self.b)
+        if amount < self.b:
+            self.b -= amount
+            print("withdrawing some money")
+            # if the user withdraws money they have in their account
+            # then the money is simply deducted from their account
+        elif amount > self.b:
+            # if the user withdraws more money than they have in their
+            # account then a check is performed to make sure they
+            # haven't gone over their overdraft limit
+            if self.b - amount > -self.ov:
+                self.b -= amount
+                print("you've withdrawn money and gone into your overdraft, you are still within the limit though")
+            else:
+                self.b -= amount
+                self.b -= 5
+                print("you've withdrawn some money and gone into your overdraft")
+        print("new amount in bank balance is ", self.b)
 
-
+    def deposit(self, amount):
+        """lets the user deposit an amount of money, adds it to their account"""
+        self.b += amount
+        print("you have deposited £", amount, "new bank balance is £", self.b)
