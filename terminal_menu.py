af42f392dd3c740f45b88fde543df4927d66d3c9
AdamLenahanNclUni <c20346427@newcastle.ac.uk>
2022-10-31 10:27:17 +0000

    created basic account creation process for the user, this version of the account creation does not contain validity checking

--- terminal_menu.py
+++ terminal_menu.py
@@ -1,7 +1,8 @@
 from customer_class import Customer
-
+from datetime import datetime
 
 def main_menu():
+    """creates an infinite loop that presents the user with a list of options for interacting with an account?"""
     stop = False
     while stop is False:
         print("would you like to add a new customer (1), delete a customer (2), edit a customers details(3),"
@@ -26,7 +27,22 @@ def main_menu():
 
 
 def create_new_account():
-    Customer(" ", " ", " ", " ", " ", " ", " ", " ")
+    """gives the user a way to create a new line in the csv file which represents a user"""
+    first_name = input("What is the first name of your user?")
+    last_name = input("What is the last name of your user? ")
+    user_title = input("What is your user's title? ")
+    preferred_pronouns = input("What are your user's preferred pronouns? ")
+    print("What date was your user born? ")
+    date_entry = input('Enter a date (i.e. dd/mm/yyyy) ')
+    try:
+        day, month, year = map(int, date_entry.split('/'))
+        date = datetime(day, month, year)
+    except ValueError:
+        print("Incorrect format")
+    user_occupation = input("What is your user's occupation? ")
+    user_balance = input("What is your user's balance? ")
+    draught_limit = input("What is your user's over-draught limit.? ")
+    Customer(first_name, last_name, user_title, preferred_pronouns, date, user_occupation, user_balance, draught_limit)
 
 
 main_menu()
