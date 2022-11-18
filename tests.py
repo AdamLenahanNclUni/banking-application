dfb547ead7f47a725f43609e3cdf7474a2d0c5ec
AdamLenahanNclUni <c2034642@newcastle.ac.uk>
2022-11-18 09:48:56 +0000

    made tests more rigorous

--- tests.py
+++ tests.py
@@ -44,39 +44,6 @@ def test_deposit():
             print("there are currently no user's with that id")
 
 
-# this test shows that the add client to csv method works
-test_add_client_to_csv()
-# this test should print out "there are currently no user's with that id" at this stage
-new_customer = Customer("101","Amandie","Tearney","Dr","she/her","1999-08-31","Chief Design Engineer","-752.53","991.48")
-Customer.add_client_to_csv(new_customer)
-test_add_client_to_csv()
-# this test should print out "['101', 'Amandie', 'Tearney', 'Dr', 'she/her', '1999-08-31', 'Chief Design Engineer',
-# '-752.53', '991.48']" at this stage.
-
-# this test tests that the withdrawal method works
-test_withdrawal()
-# this test should print "-752.53" at this stage
-# "current amount in bank balance is  -752.53"
-new_customer.withdrawal(100)
-# "you've withdrawn money and gone into your overdraft, you are still within the limit though"
-# "new amount in bank balance is  -852.53"
-
-# this test tests that the deposit method works
-new_customer.deposit(100)
-# the program should write "you have deposited £ 100.0 new bank balance is £ -752.53" to the terminal
-test_deposit()
-# the program should write "-752.53" to the terminal
-
-# this test tests that the string representation of the user is implemented correctly
-print(new_customer.__str__())
-# should print "Customer Amandie Tearney has a balance of -752.53"
-
-# this test tests the __repr__ method works correctly.
-print(new_customer.__repr__())
-# should print "Customer Dr Amandie Tearney has a balance of -752.53 their overdraught limit is 991.48.
-# They were born on -752.53. Their pronouns are she/her and they are a Chief Design Engineer"
-
-
 def reset_file():
     input_file = open('UserDetails.csv', 'r')
     output_file = open('UserDetailsDelete.csv', 'a', newline='')
@@ -108,10 +75,56 @@ def reset_file():
     f.truncate()
     f.close()
     # deleting all the values in the file we copied our original file to.
+    # this subroutine deletes the test user from the database. It is run at the end of the tests to reset the file
+    # so that tests can be run multiple times without having a user with more than one of the same id.
 
-# this subroutine resets the file so testing may be done multiple times as id may clash if it is run more than
-# once causing errors without it
+# this test shows that the add client to csv method works
+test_add_client_to_csv()
+# this test should print out "there are currently no user's with that id" at this stage
+new_customer = Customer("101","Amandie","Tearney","Dr","she/her","1999-08-31","Chief Design Engineer","-752.53","991.48")
+Customer.add_client_to_csv(new_customer)
+test_add_client_to_csv()
+# this test should print out "['101', 'Amandie', 'Tearney', 'Dr', 'she/her', '1999-08-31', 'Chief Design Engineer',
+# '-752.53', '991.48']" at this stage.
+
+# this test tests that the withdrawal method works
+test_withdrawal()
+# this test should print "-752.53" at this stage
+# "current amount in bank balance is  -752.53"
+new_customer.withdrawal(100)
+# "you've withdrawn money and gone into your overdraft, you are still within the limit though"
+# "new amount in bank balance is  -852.53"
+reset_file()
+# this is done to delete the old user details from the file which are out of date.
+Customer.add_client_to_csv(new_customer)
+#this is done to save the new details to the csv file for later use.
+test_withdrawal()
+#this should print "-852.53"
+
+test_deposit()
+#this test should print the users current balance which is unchanged from last time
+# this test proves that the new user details have been saved to the file.
+reset_file()
+# this subroutine removes old outdated user information from the file
+new_customer.deposit(100)
+# this test tests that the deposit method works
+# the program should write "you have deposited £ 100.0 new bank balance is £ -752.53" to the terminal
+Customer.add_client_to_csv(new_customer)
+# this saves the new user info to the file to be read at a later time
+test_deposit()
+# the program should write "-752.53" to the terminal
+
+
+
+# this test tests that the string representation of the user is implemented correctly
+print(new_customer.__str__())
+# should print "Customer Amandie Tearney has a balance of -752.53"
+
+# this test tests the __repr__ method works correctly.
+print(new_customer.__repr__())
+# should print "Customer Dr Amandie Tearney has a balance of -752.53 their overdraught limit is 991.48.
+# They were born on -752.53. Their pronouns are she/her and they are a Chief Design Engineer"
 
 
 reset_file()
-
+# this line of code resets the file so the tests can be run multiple times
